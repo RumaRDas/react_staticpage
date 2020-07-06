e8f114e9fe47b5189d26ebafa3bc10f5542de3d0
Ruma Das <rumaadl16@gmail.com>
2020-07-06 12:11:21 +0930

    adding footer page

--- staticpage/src/components/Footer.js
+++ staticpage/src/components/Footer.js
@@ -4,7 +4,12 @@ const Footer = () => {
 
         return (
             <Fragment>
-                <h1> Footer</h1>
+            <footer className="container-fluid text-center">
+            <a href="#myPage" title="To Top">
+              <span className="glyphicon glyphicon-chevron-up"></span>
+            </a>
+            <p>React Static Page <a href="https://rumardas.github.io/react_staticpage/." title="React Static page">Ruma</a></p>
+          </footer>
             </Fragment>
         )
 
